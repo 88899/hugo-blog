24b1bc6606c7cfe350cca85252806ed7c2a0128f
88899 <githubmen@gmail.com>
2026-01-20 12:26:58 +0800

    light

--- themes/dream/assets/js/main.js
+++ themes/dream/assets/js/main.js
@@ -4,15 +4,20 @@ document.addEventListener('alpine:init', function () {
   Alpine.store('darkMode', {
     init: function init() {
       var _this = this;
-      var isDark = window.localStorage.getItem('hugo-theme-dream-is-dark');
-      if (isDark) {
-        this.on = isDark;
-      } else {
-        this.mql.addEventListener('change', function (event) {
-          _this.on = event.matches ? 'y' : 'n';
-        });
-        this.on = 'n';
-      }
+
+      var checkAndSetTheme = function() {
+        var isDark = window.localStorage.getItem('hugo-theme-dream-is-dark');
+
+        // 如果没有设置，默认使用亮色
+        if (!isDark) {
+          isDark = 'n';
+          window.localStorage.setItem('hugo-theme-dream-is-dark', 'n');
+        }
+
+          _this.on = isDark;
+      };
+      checkAndSetTheme();
+
       setTimeout(function () {
         _this.setThemeForUtterances();
       }, 6000);
